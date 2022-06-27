8996d3017794b677a3f7d682a6515ce957f0f8e2
Sergey_Tyutvin <tyutvi@gmail.com>
2022-06-27 07:21:44 +0300

    Задача №41 решена

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+        Console.Write("Введите числа (через пробел или запятую, или tab): ");
+        char[] delimiterChars = { ' ', ',', '\t' }; //переменная типа char, которая учитывает знаки разделения между вводимыми символами в строке
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(delimiterChars), int.Parse); //преобразование строки из вводимых символов в цифры
+        int count = 0;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] > 0)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine($"Кол-во элементов > 0: {count}");
+    }
+}
